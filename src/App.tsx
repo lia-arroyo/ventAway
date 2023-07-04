66f68bd414cb213ab1840eafc514e755ebd5c24b
Lia Arroyo <liayzabel@gmail.com>
2023-07-04 18:20:52 +1200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      This is a create react app template, with the unneccessary stuff removed
+      and with typescript.
+    </div>
+  );
+}
+
+export default App;
