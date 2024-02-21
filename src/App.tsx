b7802c27416470fe40cd0a19f7cd63731539ba2c
lia-arroyo <liayzabel@gmail.com>
2024-02-21 15:48:04 +1300

    Add framer motion animation (temporary)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import { motion } from "framer-motion";
 import { Button } from "./components/Button";
 import TextField from "./components/TextField";
 import "./styles/app.scss";
 
 function App() {
   return (
-    <div className="app">
+    <motion.div
+      className="app"
+      animate={{ scale: 0 }}
+      transition={{ duration: 5 }}
+    >
       <p>vent away...</p>
       <TextField />
       <Button />
-    </div>
+    </motion.div>
   );
 }
 
