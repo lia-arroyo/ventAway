d7c03f994431871f4505e1f569af4c1348b445a8
lia-arroyo <liayzabel@gmail.com>
2024-02-21 17:27:57 +1300

    Empty text and make empty textbox reappear after clicking on button

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,28 @@ import { MotionProps, motion } from "framer-motion";
 import { Button } from "./components/Button";
 import TextField from "./components/TextField";
 import "./styles/app.scss";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { TIMEOUT_DURATION } from "./utils/consts";
 
 function App() {
   const [isButtonClicked, setIsButtonClicked] = useState<boolean>(false);
 
   const animations: MotionProps = {
     animate: { scale: 0 },
-    transition: { duration: 1.5 },
+    transition: { duration: TIMEOUT_DURATION / 1000 - 0.5 },
   };
+
+  useEffect(() => {
+    isButtonClicked &&
+      setTimeout(() => {
+        setIsButtonClicked(false);
+      }, TIMEOUT_DURATION);
+  }, [isButtonClicked]);
 
   return (
     <motion.div className="app" {...(isButtonClicked && animations)}>
       <p>vent away...</p>
-      <TextField />
+      <TextField isButtonClicked={isButtonClicked} />
       <Button onClick={() => setIsButtonClicked(true)} />
     </motion.div>
   );
